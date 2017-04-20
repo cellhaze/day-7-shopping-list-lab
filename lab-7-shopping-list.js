c97dd6649fe57548e76ce2437f6b960c98afce01
Marcell Hayes <marcell.made@gmail.com>
2017-04-19 20:28:34 -0400

    all progress and steps left commended out. Finally got items to show up in list.

--- lab-7-shopping-list.js
+++ lab-7-shopping-list.js
@@ -1,3 +1,5 @@
+
+
 var shoppingList = [
     {
     product: "eggs",
@@ -42,8 +44,45 @@ var shoppingList = [
   
 var total = 0;  
 
+shoppingList.forEach(function(i, j) {
+//    (i=0, i<11, i++)
+var list = document.getElementById("listitems");
+var listItem = document.createElement('li');
+list.appendChild(listItem, shoppingList);
+    var p = document.createTextNode("Hello");
+    list.appendChild(p);
+});
+
+var total = document.createElement(shoppingList);
 
 
+//shoppingList.forEach(function(item){
+// console.log(item.name, item.price);
+//};
+
+
+//var node = document.createElement("li");                 // Create a <li> node
+//var textnode = document.createTextNode("Water");         // Create a text node
+//node.appendChild(textnode);                              // Append the text to <li>
+//document.getElementById("list-items").appendChild("ul");     // Append <li> to <ul> with id="myList"
+
+
+
+//var itemCol = document.getElementById("list-items");
+
+//var itemCol = document.createElement("li");
+//var position = document.getElementById("ul");
+//position.appendChild(itemCol);
+
+
+//shoppingList.forEach(function(item) {
+//    document.appendChild("li");
+//                    });\
+
+
+
+
+//To calculate price and tax
 shoppingList.forEach (function(i) {
     total += i.price;
 });
@@ -51,12 +90,16 @@ var tax = (total * 0.06)
 
 console.log(tax + total);
 
+            
+                    
+                    
 
 
-// var price = 0;  
-// var total = price;
-// // this is redundant and unecessary
 
-// var total = price;
-// var price = 0; 
-// // the order on this is illogical
+/* var price = 0;  
+ var total = price;
+ this is redundant and unecessary
+
+ var total = price;
+ var price = 0; 
+ the order on this is illogical */
