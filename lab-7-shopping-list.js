3b3fe45f2a583d6d5e6d2ba44f635d665d32f928
Marcell Hayes <marcell.made@gmail.com>
2017-04-20 04:38:00 -0400

    notes added for 3 more methods tried. still no luck.

--- lab-7-shopping-list.js
+++ lab-7-shopping-list.js
@@ -41,41 +41,62 @@ var shoppingList = [
     product: "eggs",
     price: 2.00
   }];
-  
-var total = 0;  
-var ammount = shoppingList.entries
-shoppingList.forEach(function() {
 
-var list = document.getElementById("listitems");
-var listItem = document.createElement('li');
-var p = document.createTextNode("Hello" + shoppingList[0]);    
-list.appendChild(listItem, shoppingList);
-list.appendChild(p);
-});
 
+//Including "Hello" to test
+//var total = 0;  
+//var ammount = shoppingList.entries
+//shoppingList.forEach(function() {
+//
+//var list = document.getElementById("listitems");
+//var listItem = document.createElement('li');
+//var p = document.createTextNode("Hello" + shoppingList[0]);  
+//var p = document.createTextNode("Hello" + shoppingList[0]);    
+//list.appendChild(listItem, shoppingList);
+//list.appendChild(p);
+//});
+
+
+function myFunction() {
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("Onions");
+    node.appendChild(textnode);
+    document.getElementById("listitems").appendChild(node);
+
+
+//Testing
 console.log(ammount);
 
+
+//Putting these here to figure out later.
+//document.getElementById("listItems").style.backgroundColor = "black";
+//document.getElementById("listItems").style.color = "white";
+//document.getElementById("listItems").style.listStyleType = none;
+
+
+//-----(failed method 5)-----
 //var sum = total;
 //var sumEl - document.createElement("h3");
 //sumEl.appendChild(sum);
 
 
 //var total = document.createElement(shoppingList);
-//Failed to execute element
 
 
+//-----(failed method 4)-----
 //shoppingList.forEach(function(item){
 // console.log(item.name, item.price);
 //};
 
 
+//-----(failed method 3)------
 //var node = document.createElement("li");                 // Create a <li> node
 //var textnode = document.createTextNode("Water");         // Create a text node
 //node.appendChild(textnode);                              // Append the text to <li>
 //document.getElementById("list-items").appendChild("ul");     // Append <li> to <ul> with id="myList"
 
 
-
+//-----(failed method 2)-----
 //var itemCol = document.getElementById("list-items");
 
 //var itemCol = document.createElement("li");
@@ -83,9 +104,10 @@ console.log(ammount);
 //position.appendChild(itemCol);
 
 
+//-----(failed method 1)-----
 //shoppingList.forEach(function(item) {
 //    document.appendChild("li");
-//                    });\
+//                    });
 
 
 
